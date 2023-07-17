aae7e53ce32a75993bf5b62d1deea819b3f694f8
AlekseyAmp <thientaovv@gmail.com>
2023-07-17 18:20:14 +0500

    Delete max_length from date

--- src/models/rate.py
+++ src/models/rate.py
@@ -6,6 +6,6 @@ from utils.validators import PositiveNumberValidator
 
 class Rate(Model):
     id = fields.IntField(pk=True)
-    date = fields.DateField(max_length=1)
-    cargo_type = fields.CharField(max_length=1024)
+    date = fields.DateField
+    cargo_type = fields.CharField(max_length=256)
     rate = fields.FloatField(validators=[PositiveNumberValidator()])
